ec3787c181fdd08587de0dbe48148c11f21a5039
Akshita <barot.ak@northeastern.edu>
2024-02-14 22:04:27 -0500

    changes in int workflow

--- test/integration.js
+++ test/integration.js
@@ -78,10 +78,5 @@ describe('Integration tests for /v1/user endpoint', () => {
       expect(getUserResponse.body.first_name).to.equal(updateData.first_name); // Validate updated details
       expect(getUserResponse.body.last_name).to.equal(updateData.last_name);
     });
-
-        // After all tests are finished, exit with code 0 (success)
-        after(() => {
-            process.exit(0);
-          });
   });
   
