00d69019b703fb60683b00c4ecf91de700851c5e
Akshita <barot.ak@northeastern.edu>
2024-02-29 00:21:10 -0500

    changes in script

--- config/database.js
+++ config/database.js
@@ -4,10 +4,15 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize({
   dialect: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_NAME || 'webapp'
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+  // host: process.env.DB_HOST || 'localhost',
+  // username: process.env.DB_USER || 'root',
+  // password: process.env.DB_PASSWORD || 'root',
+  // database: process.env.DB_NAME || 'webapp'
+  
 });
 
 // Synchronize models with the database
